af6003f6d5765998f5aa2150d0afba79158d6880
hiteshrepo <pattanayak.hitesh03@gmail.com>
2025-05-16 16:51:23 +0530

    vector embeddings | chunk, generate embeddings and insert movie details

---
+++ vector-embeddings/langchain/ops.go
@@ -0,0 +1,15 @@
+package langchain
+
+import "github.com/tmc/langchaingo/textsplitter"
+
+func SplitDocuments(
+	splitter string,
+	document string) ([]string, error) {
+	recurCh := textsplitter.NewRecursiveCharacter(
+		textsplitter.WithSeparators([]string{splitter}),
+		textsplitter.WithChunkSize(150),
+		textsplitter.WithChunkOverlap(15),
+	)
+
+	return recurCh.SplitText(document)
+}
